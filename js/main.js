b07e9dc2bdc330a9ea35cf16638620f2d626bb68
HJ959 <henryjaames@gmail.com>
2019-12-21 11:16:48 +0000

    Added OBJ file of Metamaterial USB

--- js/main.js
+++ js/main.js
@@ -144,7 +144,7 @@ function getRandomInt(min, max) {
 
   		scene.add( object );
       object.name = "spaceShuttle";
-      console.log(object.name);
+      var spaceShuttle = scene.getObjectByName( "spaceShuttle", true );
 
   	},
   	// called when loading is in progresses
@@ -172,9 +172,7 @@ function getRandomInt(min, max) {
   line.material.opacity = 0.5;
   line.material.transparent = true;
 
-  var spaceShuttle = scene.getObjectByName( "spaceShuttle", true );
 
-  console.log(spaceShuttle);
 
   scene.add( line );
 
@@ -188,8 +186,8 @@ function getRandomInt(min, max) {
     line.rotation.x = time * 0.05;
     line.rotation.y = time * 0.01;
 
-    //spaceShuttle.rotation.x = time * 0.01;
-    //spaceShuttle.rotation.y = time * 0.03;
+    spaceShuttle.rotation.x = time * 0.01;
+    spaceShuttle.rotation.y = time * 0.03;
 
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
