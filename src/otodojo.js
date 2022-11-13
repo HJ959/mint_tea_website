f7aec0aad6c2483d4eed740778855a869b34b148
Henry James <henryjaames@gmail.com>
2022-11-13 20:54:32 +0000

    added more shapes and opacity changes

--- src/otodojo.js
+++ src/otodojo.js
@@ -22,6 +22,12 @@ const shape1 = document.getElementById('shape1')
 const shape2 = document.getElementById('shape2')
 const shape3 = document.getElementById('shape3')
 const shape4 = document.getElementById('shape4')
+
+shape1.src=`/media/shapes/shape${getRndInt(1,10)}.png`
+shape2.src=`/media/shapes/shape${getRndInt(1,10)}.png`
+shape3.src=`/media/shapes/shape${getRndInt(1,10)}.png`
+shape4.src=`/media/shapes/shape${getRndInt(1,10)}.png`
+
 let start, previousTimeStamp
 let targetX = 0
 let targetY = 0
@@ -40,12 +46,12 @@ let primeNumber4 = primeNumbers[getRndInt(0, primeNumbers.length - 1)]
 // for generating biased random numbers for the blur
 const blurValues = {
     '0': 0,
-    '-1': 1,
-    '-2': 2,
-    '-3': 4,
-    '-4': 8,
-    '-5': 16,
-    '-6': 32
+    '-1': 0,
+    '-2': 1,
+    '-3': 2,
+    '-4': 4,
+    '-5': 8,
+    '-6': 16
 }
 const blurValuesLength = Object.keys(blurValues).length
 let blurValue = 0
@@ -53,12 +59,12 @@ let blurValue = 0
 function shapeMover(shape) {
     // this function moves and randomizes some 
     // of the parameters for each shape
-    blurValue = getRndInt(0, blurValuesLength - 1)
-    shape.style.filter = `blur(${blurValues[`-${blurValue}`]}px)`
+    // blurValue = getRndInt(0, blurValuesLength - 1)
+    shape.style.opacity = `${getRndInt(8,10)*0.1}`
     let screenWidth = screen.width
     let screenHeight = screen.height
     shape.style.transform = `translateX(${getRndInt(0, screenWidth * 0.66)}px) 
-                             translateY(${getRndInt(0, screenHeight)}px)
+                             translateY(${getRndInt(0, screenHeight * 0.5)}px)
                              rotate(${getRndInt(0, screenWidth * 0.77)}deg)`
     shape.style.zIndex = `-${blurValue}`
 }
